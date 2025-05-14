4568dc1ed87d0797113f5a23aedb5f2bdaeaaea2
Agustin <68752468+agustinheberling@users.noreply.github.com>
2025-05-14 08:31:54 -0300

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,6 @@
-docker run -p 8000:8000 -p 8501:8501 chatbot-financiero-app
+docker run -p 8000:8000 -p 8501:8501 chatbot-financiero-app2
+
+# Pasar el token de hugging face como variable de entorno
+#docker run -p 8000:8000 -p 8501:8501 \
+#  -e HUGGINGFACEHUB_API_TOKEN=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 \
+#  chatbot-financiero-app2
